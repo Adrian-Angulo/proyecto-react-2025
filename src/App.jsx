b9af6016adb22f12e73a82af0cc685a4fc7d0471
AdrianCastillo <adrianangulo1080@gmail.com>
2025-08-29 11:42:11 -0500

    Cosumo de apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
-  )
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((resp) => resp.json())
+      .then((post) => {
+        setPosts(post)
+      })
+      .catch(() => console.error("La peticion fallo"))
+  }, [])
+
+  if (!posts) {
+    return <h1>...cargando</h1>
+  } else {
+    return (
+      <>
+        <h1>Lista de cripto monedas</h1>
+        <ul>
+          <>
+            {posts.map(post => (
+              <li>Nombre: {post.title}</li>
+            ))}
+          </>
+        </ul>
+      </>
+    )
+  }
+
+
 }
 
 export default App
