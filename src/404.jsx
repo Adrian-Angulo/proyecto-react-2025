431622f9a8be8381a4da5df21bb24e54a0f5d601
AdrianCastillo <adrianangulo1080@gmail.com>
2025-09-01 19:22:09 -0500

    Rutas

---
+++ src/404.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Pagina404 = () => {
+    return (
+        <>
+            <div>Pagina no encontrada
+            </div>
+            <span>la pagina solicitada no existe</span>
+        </>
+    )
+}
+
+export default Pagina404
